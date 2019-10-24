46a01a025e54f2a595a4a151abf26f724be0d155
Michel Silva <michelalmeidasilva@outlook.pt>
2019-10-24 12:44:08 -0300

    Correcao da funcao de Desviar

--- Rover.c
+++ Rover.c
@@ -1,5 +1,5 @@
   #include<stdio.h>
-  #include"auxiliar.h"
+  #include"Rover.h"
   #include <webots/distance_sensor.h>
   #include <webots/motor.h>
   #include <webots/robot.h>
@@ -8,7 +8,11 @@
   #include <stdlib.h>
   #define TIME_STEP 64
 
-  int estado = 5, avoidance_counter = 0;
+
+  int estado = 0, avoidance_counter = 0;
+  int counter_teste = 0;
+  int segundos = -1;
+  
   double leftSpeed=0, rightSpeed=0;
   WbDeviceTag leftBumper ;
   WbDeviceTag groundSensor ;
@@ -16,6 +20,7 @@
 
   int main() {
     wb_robot_init();
+    
     leftBumper = wb_robot_get_device("S1");
     groundSensor = wb_robot_get_device("S2");
     rightBumper = wb_robot_get_device("S3");
@@ -29,20 +34,24 @@
   
     wb_motor_set_position(left_motor, INFINITY);
     wb_motor_set_position(right_motor, INFINITY);
+
     wb_motor_set_velocity(left_motor, 0.0);
     wb_motor_set_velocity(right_motor, 0.0);
 
     while (wb_robot_step(TIME_STEP) != -1) {
-      imprimeSensores();
+      
+      //imprime os sensores a aproximadamente 15 * 64ms ( 1 segundo)
+      one_second();
 
       switch(estado){
+     
         case condicoes: 
-          if (avoidance_counter == 0) {
+          //Nao tem obstaculo
+          if (avoidance_counter == 0) 
             estado = sensores;
-          } else {
+          //Obstaculo
+          else
             estado = sensores;
-          }
-          estado = sensores;
           break;
 
         case sensores:
@@ -50,14 +59,26 @@
           break;
 
         case linha:
-          rightSpeed = 0.2;
-          leftSpeed = 1.0;
+
+          if(groundSensor > 30 && groundSensor < 63){
+            rightSpeed = 2.0;
+            leftSpeed = 2.0;
+            estado = condicoes;
+          }
+          
           estado = condicoes;
           break;
 
         case foraLinha:
-          rightSpeed= 1.0;
-          leftSpeed = 0.2;
+          if( groundSensor < 43){
+            rightSpeed= 0.2;
+            leftSpeed = 1.0;
+            estado = condicoes;
+          } else if(groundSensor < 23){ 
+            rightSpeed = 0.2;
+            leftSpeed = 1.0;
+            estado = condicoes;
+          }
           estado = condicoes;
           break;
 
@@ -66,8 +87,13 @@
           break;
       }
 
+      if(counter_teste  > 300)
+        counter_teste = 0;
+
       wb_motor_set_velocity(left_motor, leftSpeed);
       wb_motor_set_velocity(right_motor, rightSpeed);
+ 
+
   }
   wb_robot_cleanup();
   return 0;
@@ -79,9 +105,9 @@ int verificaSensores(){
     rightSpeed = -1;
     avoidance_counter = 2000;
     return obstaculo;
-  } else if (wb_distance_sensor_get_value(groundSensor) > 43) {
+  } else if (wb_distance_sensor_get_value(groundSensor) > 40) {
       return linha;
-  } else if((wb_distance_sensor_get_value(groundSensor) < 43)){
+  } else if((wb_distance_sensor_get_value(groundSensor) < 40)){
       return foraLinha;
   }
   return condicoes;
@@ -95,14 +121,15 @@ void imprimeSensores(){
 }
 
 void desviar(){
+
   if (avoidance_counter > 800) {
-    leftSpeed = -1.0;
-    rightSpeed = -1.0;
+    leftSpeed = -0.6;
+    rightSpeed = -1;
   } else if (avoidance_counter > 600) {
-    leftSpeed = 1.3;
-    rightSpeed = 1.3;
+    leftSpeed = 1.5;
+    rightSpeed = 1.5;
   } else if (avoidance_counter > 70) {
-    leftSpeed = -0.7 ;
+    leftSpeed = 0.7 ;
     rightSpeed = 1;
     if (wb_distance_sensor_get_value(groundSensor) > 43){
       avoidance_counter = 1;
@@ -112,7 +139,17 @@ void desviar(){
     leftSpeed = 5;
     rightSpeed = 5;
     if (wb_distance_sensor_get_value(groundSensor) > 43)
-      avoidance_counter = 1;
+    avoidance_counter = 1;
   }
   avoidance_counter--;
+}
+
+
+void one_second(){
+   if( counter_teste % 15 == 1 ) {
+        imprimeSensores();
+        segundos++;
+        printf("segundos = %d\n", segundos);
+      }          
+    counter_teste++ ;
 }
--- Rover.h
+++ Rover.h
@@ -1,11 +1,17 @@
 void imprimeSensores();
 void desviar();
 int verificaSensores();
-
+void one_second();
 enum estados_robo{
+  condicoes = 0,
   linha = 1,
-  obstaculo = 2,
-  sensores = 4,
-  condicoes = 5,
-  foraLinha = 6
+  foraLinha = 2,
+  obstaculo = 3,
+  sensores = 4
 };
+
+
+
+
+
+
