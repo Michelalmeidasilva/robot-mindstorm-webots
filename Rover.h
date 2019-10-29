7ac755769cc56cff629ced4ff8ead72b31523e39
Michel Silva <37683337+Michelalmeidasilva@users.noreply.github.com>
2019-10-29 15:57:09 -0300

    Update Rover.h

--- Rover.h
+++ Rover.h
@@ -2,13 +2,26 @@ void imprimeSensores();
 void desviar();
 int verificaSensores();
 void one_second();
+
+#define sensor_i  43
 enum estados_robo{
   condicoes = 0,
   linha = 1,
-  foraLinha = 2,
+  reajuste = 2,
   obstaculo = 3,
   sensores = 4
 };
+
+// struct {
+//   int estado;
+//   int avoidance_counter;
+//   int contaSegundos;
+// } typedef controle_robo;
+
+// struct {
+//   double leftSpeed;
+//   double rightSpeed;
+// } typedef motores;
 
 
 
